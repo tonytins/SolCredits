ac4dc0c66170c30b20b1df0537dc0b193e01dc2d
Tony Bark <noreply@tonybark.com>
2024-08-03 13:28:18 -0400

    Income is now capacity divided by the volts generated Removed wallet text and symbol in favor of icon Assets are now in the public domain

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,12 +1,26 @@
 extends Control
 
-var battery_percentage: float = 0.0
-var wallet: int = 168
-var generating_energy: bool = true
-@export var capacity = 500
+const wallet_limit: float = 1000
+
+@export var starting_funds: float = 168
+## How many volts is generated per-cycle
+@export var volts_generated: float = 10
+## Starting battery capacity
+@export var battery_capacity: float = 500
+@export var budget_minimum: float = 300
+## How low the battery gets before resuming production
+@export var battery_minimum: float = 150
+@export var battery_consumption: float = 5
+
 @onready var wallet_ui_val = $MenuBar/Wallet/WalletVal
 @onready var battery_ui_bar = $MenuBar/BatteryBar
-@onready var capacity_ui_val = $MenuBar/BatteryCap/BatCapVal
+@onready var battery_capacity_ui_val = $MenuBar/BatteryCap/BatCapVal
+
+var battery_percentage: float = 0
+var wallet: float = starting_funds
+var generating_energy: bool = true
+var earned_income:
+	get: return battery_capacity / volts_generated
 
 func _ready():
 	randomize()
@@ -16,45 +30,47 @@ func _process(delta):
 		generate_energy(delta)
 	else:
 		consume_energy(delta)
+
 	check_wallet()
 	display_status()
 
+## Generate and store energy 
+## until battery reaches battery_capacity
 func generate_energy(delta):
-	if battery_percentage < capacity:
-		battery_percentage += 10.0 * delta
-		if battery_percentage >= capacity:
-			battery_percentage = capacity
+	if battery_percentage < battery_capacity:
+		battery_percentage += volts_generated * delta
+		if battery_percentage >= battery_capacity:
+			battery_percentage = battery_capacity
 			generating_energy = false
 			add_to_wallet()
 	else:
 		generating_energy = false
 
+## Battery consumes eneregy until
 func consume_energy(delta):
-	const low_battery = 150.0
-	battery_percentage -= 5.0 * delta
-	if battery_percentage <= low_battery:
-		battery_percentage = low_battery
+	battery_percentage -= battery_consumption * delta
+	if battery_percentage <= battery_minimum:
+		battery_percentage = battery_minimum
 		generating_energy = true
 
 func add_to_wallet():
-	var earned_credits = randf_range(100, 200)
-	wallet += earned_credits
-	if wallet > 1000:
-		wallet = 1000
-	elif wallet > 500:
+	wallet += earned_income
+	if wallet > wallet_limit:
+		wallet = wallet_limit
+	## Simulate spending
+	elif wallet > budget_minimum:
 		wallet -= sell_energy(wallet - 300)
 
+## Simulate selling the energy for credits
 func sell_energy(amount):
-	# Simulate selling the energy for credits
 	return int(amount * 0.5)
 
+## Ensure the wallet does not exceed the limit
 func check_wallet():
-	# Ensure the wallet does not exceed the limit
-	if wallet > 1000:
-		wallet = 1000
+	if wallet > wallet_limit:
+		wallet = wallet_limit
 
 func display_status():
 	wallet_ui_val.text = str(wallet)
 	battery_ui_bar.value = battery_percentage
-	capacity_ui_val.text = str(capacity)
-	# print("Battery: " + str(battery_percentage) + "%, Wallet: " + str(wallet) + " Credits")
+	battery_capacity_ui_val.text = str(battery_capacity)
