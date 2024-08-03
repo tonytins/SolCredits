d2954dee83fceec769a57cff5409531b91d02d3a
Tony Bark <noreply@tonybark.com>
2024-08-03 04:05:22 -0400

    Initial commit

---
+++ Main.gd
@@ -0,0 +1,57 @@
+extends Control
+
+var battery_percentage: float = 0.0
+var wallet: int = 168
+var generating_energy: bool = true
+@onready var wallet_ui_val = $Wallet/WalletVal
+@onready var battery_ui_bar = $BatteryBar
+
+func _ready():
+	randomize()
+
+func _process(delta):
+	if generating_energy:
+		generate_energy(delta)
+	else:
+		consume_energy(delta)
+	check_wallet()
+	print_status()
+
+func generate_energy(delta):
+	if battery_percentage < 500.0:
+		battery_percentage += 10.0 * delta
+		if battery_percentage >= 500.0:
+			battery_percentage = 500.0
+			generating_energy = false
+			add_to_wallet()
+	else:
+		generating_energy = false
+
+func consume_energy(delta):
+	const low_battery = 150.0
+	battery_percentage -= 5.0 * delta
+	if battery_percentage <= low_battery:
+		battery_percentage = low_battery
+		generating_energy = true
+
+func add_to_wallet():
+	var earned_credits = randf_range(100, 200)
+	wallet += earned_credits
+	if wallet > 1000:
+		wallet = 1000
+	elif wallet > 500:
+		wallet -= sell_energy(wallet - 300)
+
+func sell_energy(amount):
+	# Simulate selling the energy for credits
+	return int(amount * 0.5)
+
+func check_wallet():
+	# Ensure the wallet does not exceed the limit
+	if wallet > 1000:
+		wallet = 1000
+
+func print_status():
+	wallet_ui_val.text = str(wallet)
+	battery_ui_bar.value = battery_percentage
+	# print("Battery: " + str(battery_percentage) + "%, Wallet: " + str(wallet) + " Credits")
---
+++ README.md
@@ -0,0 +1,13 @@
+# Solar Credits
+
+Made in Godot 4.x and inspired by Stellaris, Solar Credits explores the concept of earning money using renewable energy to earn Credits (**Ȼ**). This prototype is fairly simple in design. You own a simple household that has solar panels. Energy production temporarily stops once you reach 100%. You earn the sum of the total energy stored in your battery, which is consumed until it reaches a certain minimum threshold, and you resume producing energy again.
+
+In the future, I'll add a day-night cycle with the potential to improve reliability by buying batteries or panels to produce and store more. However, the core game loop is functional.
+
+## Surplus of Potential
+
+While much of this premise is fictional, don't get me wrong, this is technically already a feasible today. You can sell surplus for money in real life. Actually, it is a good motivation to get solar panels in the first place. So it isn't that far-fetched. This game just expands upon that concept by making the very act of generating energy your universal basic income instead. Selling the surplus shouldn't be an option, but a guarantee.
+
+## License
+
+I license this project under the GPL-3.0 license - see [LICENSE](LICENSE) for details.
